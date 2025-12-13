5e50fd3a49bb30fe0154c250568afedc60bff43b
Jared Pomranky <jared@netprofitmarketing.com>
2025-12-13 13:12:44 -0600

    Update staging-bot-block.php

--- staging-bot-block.php
+++ staging-bot-block.php
@@ -7,8 +7,8 @@ Author: Net Profit Marketing
 Author URI: https://www.netprofitmarketing.com
 Requires at least: 4.6
 Tested up to: 6.9
-Stable tag: 1.0.0
-Version: 1.0.0
+Stable tag: 1.0.1
+Version: 1.0.1
 Requires PHP: 7.2
 Text Domain: staging-bot-block
 License: GPL v2 or later
